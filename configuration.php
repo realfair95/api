9d1ca63699971dfa7b103cee5154fd34eff2a29c
samuel <samuelmanikiza@gmail.com>
2018-11-22 19:53:15 +0200

    Changed

--- configuration.php
+++ configuration.php
@@ -2,11 +2,6 @@
 $con=mysqli_connect("localhost","root","2016!Kongolo!");
 if($con){
 	$db=mysqli_select_db($con,"Web");
-	if($db){
-		echo "connected";
-	}else{
-		mysql_error($con);
-	}
 }else{
 
  die("error");
